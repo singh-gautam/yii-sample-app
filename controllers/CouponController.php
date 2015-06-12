89ea685d90c8aed0c8af8186d5984d6350cbc38e
Gautam Singh <gautam.singh@coupondunia.in>
2015-06-12 16:33:34 +0530

    Added coupon and deals listing

--- controllers/CouponController.php
+++ controllers/CouponController.php
@@ -2,11 +2,35 @@
 
 namespace app\controllers;
 
-class CouponController extends \yii\web\Controller
+use yii\web\Controller;
+use app\models\Coupon;
+
+class CouponController extends Controller
 {
     public function actionIndex()
     {
-        return $this->render('index');
+		$coupons = Coupon::find()
+			->where(['isDeal' => 0])
+			->andWhere(['IS NOT', 'CouponCode', null])
+			->andWhere(['!=', "CouponCode", ''])
+			->orderBy('CouponCode')
+			->limit(60)
+			->all();
+
+
+    	$deals = Coupon::find()
+    		->where(['isDeal' => 1])
+    		->andWhere(['IS NOT', 'CouponCode', null])
+    		->andWhere(['!=', "CouponCode", ''])
+    		->orderBy('CouponCode')
+    		->limit(60)
+    		->all();
+		
+        
+        return $this->render('index', [
+        	'deals' => $deals, 
+        	'coupons' => $coupons]
+        	);
     }
-    
+
 }
--- views/coupon/index.php
+++ views/coupon/index.php
@@ -1,9 +1,24 @@
 <?php
 /* @var $this yii\web\View */
 ?>
-<h1>coupon/index</h1>
+<h1>Coupons</h1>
+<ol>
+	<?php foreach($coupons as $coupon) { ?>
+	<li>
+		<strong>Code: </strong> <u> <?= $coupon->CouponCode .' <br>' ?> </u>
+		<strong>ID: </strong> <u> <?= $coupon->CouponID .'<br>' ?> </u>
+		<strong>Success: </strong> <u> <?= $coupon->CountSuccess ?> </u>
+	</li>
+	<?php } ?>
+</ol>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+<h1>Deals</h1>
+<ol>
+	<?php foreach($deals as $deal) { ?>
+	<li>
+		<strong>Code: </strong> <u> <?= $deal->CouponCode .' <br>' ?> </u>
+		<strong>ID: </strong> <u> <?= $deal->CouponID .'<br>' ?> </u>
+		<strong>Success: </strong> <u> <?= $deal->CountSuccess ?> </u>
+	</li>
+	<?php } ?>
+</ol>
